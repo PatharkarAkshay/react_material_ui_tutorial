d778b7fd6996753bb2cecdf74706a02d854ef12b
Akshay Patharkar <akshaypatharkar2013@gmail.com>
2022-03-08 12:06:40 +0530

    Button

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,65 @@
+import { Button, Stack, IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
+const MuiButton = () => {
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          disableRipple
+          onClick={() => alert("clicked")}
+          startIcon={<SendIcon />}
+        >
+          Send
+        </Button>
+        <Button variant="contained" disableElevation endIcon={<SendIcon />}>
+          Send
+        </Button>
+        <IconButton color="success" size="small" aria-label="send">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiButton;
